a71f4eacdc2bde1469bf3bf0b847c88e7e554de5
freewind <nowindlee@gmail.com>
2019-05-18 15:03:16 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-TypeScript "xml2js" Demo
-===========================
+TypeScript "xml2js" Options "explicitArray" Demo
+================================================
 
-使用`xml2js`将xml与js的object进行互转。
+xml2js中有一个选项`explicitArray`（默认为false），可以控制由xml转成的obj中，出现的单个元素是否以array的形式展示。
+由于从xml的结构中，我们无法分辨某个元素是否只能出现一次还是恰好只出现了一次，所以总是弄成array会比较安全，但是使用起来很繁琐，所以有时候需要手动控制。
+
+我个人倾向于把explicitArray设为`true`，同时对可能为array的字段再进行手动的类型定义：`Item | Item[]`
 
 ```
 npm install
--- hello.ts
+++ hello.ts
@@ -1,20 +1,48 @@
 import {parseString, Builder} from 'xml2js';
 
-function parseXmlToObject() {
-  console.log('--- parseXmlToObject ---');
-  parseString(` <name>hello</name>`, (err, result) => {
-    console.log(err, result);
-    // result:
-    // { name: 'xml2js' }
+function parseXmlToObject(xml: string, explicitArray: boolean): any {
+  let result = null;
+  parseString(xml, {explicitArray}, (err, _result) => {
+    result = _result
   })
+  return result;
 }
 
-function buildObjectToXml() {
-  console.log('--- buildObjectToXml ---');
-  const builder = new Builder()
-  const xml = builder.buildObject({name: 'world'})
-  console.log(xml)
+function buildObjectToXml(obj: any, explicitArray: boolean): string {
+  const builder = new Builder({explicitArray})
+  const xml = builder.buildObject(obj)
+  return xml;
 }
 
-parseXmlToObject();
-buildObjectToXml();
+function parseAndBuild(xml: string, explicitArray: boolean) {
+  const obj = parseXmlToObject(xml, explicitArray);
+  const builtXml = buildObjectToXml(obj, explicitArray)
+
+  console.log('obj: ', JSON.stringify(obj, null, 4));
+  console.log('builtXml: ', builtXml)
+}
+
+const xml = `<?xml version="1.0" encoding="utf-16"?>
+<SearchReport>
+    <DisplayFields>
+        <DisplayField>21234</DisplayField>
+        <DisplayField>21230</DisplayField>
+        <DisplayField>21241</DisplayField>
+        <DisplayField>21249</DisplayField>
+        <DisplayField>21263</DisplayField>
+        <DisplayField>21264</DisplayField>
+        <DisplayField>21244</DisplayField>
+        <DisplayField>21245</DisplayField>
+        <DisplayField>25998</DisplayField>
+        <DisplayField>21236</DisplayField>
+        <DisplayField>21262</DisplayField>
+    </DisplayFields>
+    <PageSize>50</PageSize>
+</SearchReport>
+`;
+
+console.log('--------- parseAndBuild: true --------------')
+parseAndBuild(xml, true);
+
+console.log('--------- parseAndBuild: false --------------')
+parseAndBuild(xml, false);
